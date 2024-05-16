a346e34e3f0703396704f18f73aa781e72a0dba7
Wisdom <elijahwidom6@gmail.com>
2024-05-15 23:36:06 -0500

    Test computer choice function
    Tested Computer choice function after assigning its returned value to a variable

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 function getComputerChoice(){
     let weapons = ["rock","paper","scissors"]
     const CHOICE = Math.floor(Math.random() * 3);
+    return  weapons[CHOICE]
 }
 
+let computerChoice = getComputerChoice()
+console.log(computerChoice)
