1a7dd2eca08b8c80be08b1fa2d46b15d40d6805c
Wisdom <elijahwidom6@gmail.com>
2024-05-16 00:39:11 -0500

    Create function to play a round
    Created the playRound function which simulates a round, if the human choice beats the computer choice then a point is added to the human and vice versa, tested

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+var humanScore = 0
+var computerScore = 0
+
+
 function getComputerChoice(){
     let weapons = ["rock","paper","scissors"]
     const CHOICE = Math.floor(Math.random() * 3)
@@ -9,10 +13,33 @@ function getHumanChoice(){
     return CHOICE
 }
 
+function playRound(humanChoice, computerChoice){
+    var playerChoice = humanChoice.toLowerCase();
+    
+    if(playerChoice == computerChoice){
+        console.log("You dare draw with me human, WE GO AGAIN!")
+        playerChoice = getHumanChoice().toLowerCase()
+        computerChoice = getComputerChoice()
+        playRound(playerChoice,computerChoice)
+    }else if(playerChoice == "rock" && computerChoice == "scissors"){
+        console.log("You win! "+playerChoice+" beats "+computerChoice)
+        humanScore++
+    }else if(playerChoice == "paper" && computerChoice == "rock"){
+        console.log("You win! "+playerChoice+" beats "+computerChoice)
+        humanScore++
+    }else if(playerChoice == "scissors" && computerChoice == "paper"){
+        console.log("You win! "+playerChoice+" beats "+computerChoice)
+        humanScore++
+    }else{
+        console.log("You Lose! "+computerChoice+" beats "+"rock")
+        computerScore++;
+    }
+}
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice()
 
-// let computerChoice = getComputerChoice()
-let humanChoice = getHumanChoice();
-console.log(humanChoice)
-// console.log(computerChoice)
+playRound(humanSelection, computerSelection)
+
+console.log(humanScore, computerScore, computerSelection)
 
