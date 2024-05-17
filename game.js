dc53b68e58fd6f69b4bb74d2491c90fd2ddd63c2
Wisdom <elijahwidom6@gmail.com>
2024-05-17 17:18:31 -0500

    Added two functions to display messages
    Added a win and lose round function to display messages depending on if the player wins or the computer wins

--- game.js
+++ game.js
@@ -9,23 +9,26 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     const CHOICE = prompt("Make your choice!")
-    proceed = false
     if((CHOICE.toLowerCase() == "rock") || (CHOICE.toLowerCase() == "scissors") || (CHOICE.toLowerCase() == "paper")){
-        // proceed = true
-        return CHOICE
-        
+        return CHOICE    
     }else{
         return getHumanChoice()
-    
     }
-    while(!proceed){
-    }
-    
 }
 
 
+function winRoundMessage(playerChoice, computerChoice, playerScore, computerScore){
+    alert("You win! "+playerChoice+" beats "+computerChoice + "\nPlayer Score:  " + playerScore + "\nComputer score: " + computerScore)
+}
+
+function loseRoundMessage(playerChoice, computerChoice, playerScore, computerScore){
+    alert("You Lose! "+computerChoice+" beats "+playerChoice + "\nHuman Score:  " + playerScore + " Computer score: " + computerScore)
+}
+
+
+
 function playGame(){
-    var humanScore = 0
+    var playerScore = 0
     var computerScore = 0
     
     while(rounds < 5){
@@ -34,35 +37,37 @@ function playGame(){
         playRound(humanSelection, computerSelection)
     }
 
+
+
     function playRound(humanChoice, computerChoice){
         var playerChoice = humanChoice.toLowerCase();
         if(playerChoice == computerChoice){
-            alert("You dare draw with me human, WE GO AGAIN!\n" + "Human Score:  " + humanScore + "\nComputer score: " + computerScore)
+            alert("You dare draw with me human, WE GO AGAIN!\n" + "Human Score:  " + playerScore + "\nComputer score: " + computerScore)
             playerChoice = getHumanChoice().toLowerCase()
             computerChoice = getComputerChoice()
             playRound(playerChoice,computerChoice)
         }else if(playerChoice == "rock" && computerChoice == "scissors"){
-            humanScore++
-            alert("You win! "+playerChoice+" beats "+computerChoice + "\nHuman Score:  " + humanScore + "\nComputer score: " + computerScore)
+            playerScore++
+            winRoundMessage(playerChoice, computerChoice, playerScore, computerScore)
             rounds++
         }else if(playerChoice == "paper" && computerChoice == "rock"){
-            humanScore++
-            alert("You win! "+playerChoice+" beats "+computerChoice + "\nHuman Score:  " + humanScore + "\nComputer score: " + computerScore)
+            playerScore++
+            winRoundMessage(playerChoice, computerChoice, playerScore, computerScore)
             rounds++
         }else if(playerChoice == "scissors" && computerChoice == "paper"){
-            humanScore++
-            alert("You win! "+playerChoice+" beats "+computerChoice + "\nHuman Score:  " + humanScore + "\nComputer score: " + computerScore)
-            rounds++
+            playerScore++
+            winRoundMessage(playerChoice, computerChoice, playerScore, computerScore)
         }else{
             computerScore++;
-            alert("You Lose! "+computerChoice+" beats "+playerChoice + "\nHuman Score:  " + humanScore + "\nComputer score: " + computerScore)
+            loseRoundMessage(playerChoice, computerChoice, playerScore, computerScore)
             rounds++
         }
        
     }
     
-    if(humanScore>computerScore){
+    if(playerScore>computerScore){
         alert("You will not have the last laugh, challenge me again and I won't make it easy")
+        
     }else{
         alert("Computers will always be smarter, better luck next time ;)")
     }
