907dd380f87370d3cb232697822eecc3546c460b
Wisdom <elijahwidom6@gmail.com>
2024-05-17 17:00:47 -0500

    Fix error that allows for invalid input
    Restricted user input to just rock, paper and scissors so any other input prompts with a make a choice
    Added trackers to round message alert that tracks the scores of computer and player

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 var rounds = 0
+var proceed
 
 function getComputerChoice(){
     let weapons = ["rock","paper","scissors"]
@@ -8,7 +9,18 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     const CHOICE = prompt("Make your choice!")
-    return CHOICE
+    proceed = false
+    if((CHOICE.toLowerCase() == "rock") || (CHOICE.toLowerCase() == "scissors") || (CHOICE.toLowerCase() == "paper")){
+        // proceed = true
+        return CHOICE
+        
+    }else{
+        return getHumanChoice()
+    
+    }
+    while(!proceed){
+    }
+    
 }
 
 
@@ -24,29 +36,29 @@ function playGame(){
 
     function playRound(humanChoice, computerChoice){
         var playerChoice = humanChoice.toLowerCase();
-        
         if(playerChoice == computerChoice){
-            alert("You dare draw with me human, WE GO AGAIN!")
+            alert("You dare draw with me human, WE GO AGAIN!\n" + "Human Score:  " + humanScore + "\nComputer score: " + computerScore)
             playerChoice = getHumanChoice().toLowerCase()
             computerChoice = getComputerChoice()
             playRound(playerChoice,computerChoice)
         }else if(playerChoice == "rock" && computerChoice == "scissors"){
-            alert("You win! "+playerChoice+" beats "+computerChoice)
             humanScore++
+            alert("You win! "+playerChoice+" beats "+computerChoice + "\nHuman Score:  " + humanScore + "\nComputer score: " + computerScore)
             rounds++
         }else if(playerChoice == "paper" && computerChoice == "rock"){
-            alert("You win! "+playerChoice+" beats "+computerChoice)
             humanScore++
+            alert("You win! "+playerChoice+" beats "+computerChoice + "\nHuman Score:  " + humanScore + "\nComputer score: " + computerScore)
             rounds++
         }else if(playerChoice == "scissors" && computerChoice == "paper"){
-            alert("You win! "+playerChoice+" beats "+computerChoice)
             humanScore++
+            alert("You win! "+playerChoice+" beats "+computerChoice + "\nHuman Score:  " + humanScore + "\nComputer score: " + computerScore)
             rounds++
         }else{
-            alert("You Lose! "+computerChoice+" beats "+playerChoice)
             computerScore++;
+            alert("You Lose! "+computerChoice+" beats "+playerChoice + "\nHuman Score:  " + humanScore + "\nComputer score: " + computerScore)
             rounds++
         }
+       
     }
     
     if(humanScore>computerScore){
