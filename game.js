8f2ba9af0969c1ba4c28cab13b8d58079a23fe23
Wisdom <elijahwidom6@gmail.com>
2024-07-31 23:00:16 -0500

    Create buttons, add event listener to buttons, update index.html to include buttons, game.js changes: getPlayerChoice has button event listeners,playRound function no longer nested, player and computer scores global vars

--- game.js
+++ game.js
@@ -1,21 +1,6 @@
 var rounds = 0
-var proceed
-
-function getComputerChoice(){
-    const weapons = ["rock","paper","scissors"]
-    const CHOICE = Math.floor(Math.random() * 3)
-    return  weapons[CHOICE]
-}
-
-function getPlayerChoice(){
-    const CHOICE = prompt("Make your choice!")
-    if((CHOICE.toLowerCase() == "rock") || (CHOICE.toLowerCase() == "scissors") || (CHOICE.toLowerCase() == "paper")){
-        return CHOICE    
-    }else{
-        return getPlayerChoice()
-    }
-}
-
+var playerScore = 0
+var computerScore = 0
 
 function winRoundMessage(playerChoice, computerChoice, playerScore, computerScore){
     alert("You win! "+playerChoice+" beats "+computerChoice + "\nPlayer Score:  " + playerScore + "\nComputer score: " + computerScore)
@@ -26,57 +11,79 @@ function loseRoundMessage(playerChoice, computerChoice, playerScore, computerSco
 }
 
 
+function getComputerChoice(){
+    const weapons = ["rock","paper","scissors"]
+    const CHOICE = Math.floor(Math.random() * 3)
+    return  weapons[CHOICE]
+}
+
+function getPlayerChoice(){
+    // if(rounds==5){
+    //     var replay = prompt("Do you want to play again").toLowerCase();
+    // }
+
+
+    var playerChoice;
+
+    var rockBtn = document.querySelector("#rockBtn");
+    var paperBtn = document.querySelector('#paperBtn');
+    var scissorsBtn = document.querySelector('#scissorsBtn');
+
+
+    rockBtn.addEventListener("click", () => {
+        playerChoice = 'rock';
+        playRound(playerChoice, getComputerChoice());
+    })
+
+    paperBtn.addEventListener("click", ()=> {
+        playerChoice = 'paper';
+        playRound(playerChoice, getComputerChoice());
+    })  
+    
+    scissorsBtn.addEventListener("click", ()=> {
+        playerChoice = 'scissors';
+        playRound(playerChoice, getComputerChoice());
+    })
+
+
+    console.log("i see you")
+}
+
+
+
+
+
+function playRound(playerChoice, computerChoice){
+    if(playerChoice == computerChoice){
+        alert("You dare draw with me human, WE GO AGAIN!\n" + "Human Score:  " + playerScore + "\nComputer score: " + computerScore)
+    }else if(playerChoice == "rock" && computerChoice == "scissors"){
+        playerScore++
+        winRoundMessage(playerChoice, computerChoice, playerScore, computerScore)
+        rounds++
+    }else if(playerChoice == "paper" && computerChoice == "rock"){
+        playerScore++
+        winRoundMessage(playerChoice, computerChoice, playerScore, computerScore)
+        rounds++
+    }else if(playerChoice == "scissors" && computerChoice == "paper"){
+        playerScore++
+        winRoundMessage(playerChoice, computerChoice, playerScore, computerScore)
+        rounds++
+    }else{
+        computerScore++;
+        loseRoundMessage(playerChoice, computerChoice, playerScore, computerScore)
+        rounds++
+    }
+}
+
 
 function playGame(){
-    var playerScore = 0
-    var computerScore = 0
+    getPlayerChoice();
 
-    const rockbtn = document.querySelector('#rock-btn');
-    const paperbtn = document.querySelector('#paper-btn');
-    const scissorsbtn = document.querySelector('#scissors-btn');
-
-    
-    
-    
-    function playRound(humanChoice, computerChoice){
-        var playerChoice = humanChoice.toLowerCase();
-        if(playerChoice == computerChoice){
-            alert("You dare draw with me human, WE GO AGAIN!\n" + "Human Score:  " + playerScore + "\nComputer score: " + computerScore)
-            playerChoice = getPlayerChoice().toLowerCase()
-            computerChoice = getComputerChoice()
-            playRound(playerChoice,computerChoice)
-        }else if(playerChoice == "rock" && computerChoice == "scissors"){
-            playerScore++
-            winRoundMessage(playerChoice, computerChoice, playerScore, computerScore)
-            rounds++
-        }else if(playerChoice == "paper" && computerChoice == "rock"){
-            playerScore++
-            winRoundMessage(playerChoice, computerChoice, playerScore, computerScore)
-            rounds++
-        }else if(playerChoice == "scissors" && computerChoice == "paper"){
-            playerScore++
-            winRoundMessage(playerChoice, computerChoice, playerScore, computerScore)
-            rounds++
-        }else{
-            computerScore++;
-            loseRoundMessage(playerChoice, computerChoice, playerScore, computerScore)
-            rounds++
-        }
-       
-    }
-    
-    if(playerScore>computerScore){
-        alert("You will not have the last laugh, challenge me again and I won't make it easy")
-    }else{
-        alert("Computers will always be smarter, better luck next time ;)")
-    }
-
-    var replay = prompt("Do you want to play again").toLowerCase()
-
-    if(replay == "y" || replay == "yes"){
-        rounds = 0
-        playGame()
-    }
+    console.log("i see you at "+ rounds)
+    // if(replay == "y" || replay == "yes"){
+    //     rounds = 0
+    //     playGame()
+    // }
 }
 
 
