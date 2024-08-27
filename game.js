6f6b31787ade40321bdbb49f08ee4e74c83571d6
Wisdom <elijahwidom6@gmail.com>
2024-08-26 20:59:06 -0500

    Create functions to disable and enable overlay and endgame modal

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ var rounds = 0
 var playerScore = 0
 var computerScore = 0
 
+const pScore = document.getElementById("playerScore");
+const cScore = document.getElementById("computerScore");
 
 function getComputerChoice() {
     const weapons = ["rock", "paper", "scissors"]
@@ -10,72 +12,86 @@ function getComputerChoice() {
 }
 
 
-function updatePlayerScore(){
+function updatePlayerScore() {
     playerScore++
-    const pScore = document.getElementById("playerScore")
-    pScore.textContent = "Player: "+playerScore
+    pScore.textContent = "Player: " + playerScore
     rounds++
 }
 
-function updateComputerScore(){
+function updateComputerScore() {
     computerScore++;
-    const cScore = document.getElementById("computerScore")
-    cScore.textContent = "Computer: "+computerScore;
+    cScore.textContent = "Computer: " + computerScore;
     rounds++
 }
 
 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const restartBtn = document.querySelector(".restartBtn");
 
 function replayGame() {
-    const result = document.getElementById("endGame");
+    enablePlayAgain();
+    const result = document.querySelector("h3.result");
     if (playerScore > computerScore) {
-        result.textContent = "You will not have the last laugh, challenge me again and I won't make it easy"
+        result.textContent = "You won, you're better than the average AI";
     } else {
-        result.textContent = "Computers will always be smarter, better luck next time";
+        result.textContent = "You lost, better luck next time!";
     }
-    // const overlay = document.
-    // enablePlayAgain();
-
-    // var replay = prompt("Do you want to play again?").toLowerCase()
-    // if (replay == "yes" || replay == "y") {
-    //     rounds = 0
-    //     playerScore = 0
-    //     computerScore = 0
-    // }
+    
+    restartBtn.addEventListener("click", resetGame);
+    return;
 }
 
 
 
-//make two functions, one to turn on the overlay and one to turn off, the overlay should have a button that can let the player restart the game
 
-function enablePlayAgain(){
-    // document.getElementById("restart").style.visibility = "visible";
 
+function enablePlayAgain() {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
 }
 
 
-function disablePlayAgain(){
-    // document.getElementById("restart").style.visibility = "hidden";
+function disablePlayAgain() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+
+function resetGame(){
+    var round = document.querySelector("h2.round");
+    round.textContent = "MAKE YOUR CHOICE";
+    playerScore = 0;
+    computerScore = 0;
+    rounds = 0;
+    pScore.textContent = "Player: " + playerScore;
+    cScore.textContent = "Computer: " + computerScore;
+    disablePlayAgain();
+
 }
 
 
 
 function playRound(playerChoice, computerChoice) {
-    var result = document.querySelector("h2.result");
+    var round = document.querySelector("h2.round");
     if (playerChoice == computerChoice) {
-        result.textContent = "Its a draw!!"
+        round.textContent = "Its a draw!!"
     } else if (playerChoice == "rock" && computerChoice == "scissors") {
-        result.textContent = "Rock beats Scissors!"
+        round.textContent = "Rock beats Scissors!"
         updatePlayerScore();
     } else if (playerChoice == "paper" && computerChoice == "rock") {
-        result.textContent = "Paper beats Rock!"
+        round.textContent = "Paper beats Rock!"
         updatePlayerScore();
     } else if (playerChoice == "scissors" && computerChoice == "paper") {
-        result.textContent = "Scissors beats paper!"
+        round.textContent = "Scissors beats paper!"
         updatePlayerScore();
     } else {
-        result.textContent = computerChoice + " beats " + playerChoice
+        round.textContent = computerChoice + " beats " + playerChoice
         updateComputerScore();
+    }
+
+    if(rounds === 5){
+        replayGame();
     }
 
 }
@@ -99,11 +115,6 @@ function playGame() {
                 break;
             default:
                 break;
-        }
-
-        if (rounds == 5) {
-            replayGame()
-
         }
     }
 
